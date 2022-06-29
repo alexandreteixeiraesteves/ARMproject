25f6ddd2b1e3ad040a21e67c5899ffa485c9d7d0
Pierre-Olivier KOENIG <pierre-olivier.koenig@epita.fr>
2022-06-29 12:08:56 +0200

    [FLASH] : First attemp to write on flash memory

--- ARMproject/Core/Src/main.c
+++ ARMproject/Core/Src/main.c
@@ -90,6 +90,68 @@ static void MX_USART2_UART_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+void Flash_Read_Data (uint32_t StartPageAddress, uint32_t *RxBuf, uint16_t numberofwords)
+{
+	while (1)
+	{
+
+		*RxBuf = *(__IO uint32_t *)StartPageAddress;
+		StartPageAddress += 4;
+		RxBuf++;
+		if (!(numberofwords--)) break;
+	}
+}
+
+uint32_t Flash_Write_Data (uint32_t StartPageAddress, uint32_t *Data, uint16_t numberofwords)
+{
+
+	static FLASH_EraseInitTypeDef EraseInitStruct;
+	uint32_t PAGEError;
+	int sofar=0;
+
+	  /* Unlock the Flash to enable the flash control register access *************/
+	   HAL_FLASH_Unlock();
+
+	   /* Erase the user Flash area*/
+
+	  uint32_t StartPage = GetPage(StartPageAddress);
+	  uint32_t EndPageAdress = StartPageAddress + numberofwords*4;
+	  uint32_t EndPage = GetPage(EndPageAdress);
+
+	   /* Fill EraseInit structure*/
+	   EraseInitStruct.TypeErase   = FLASH_TYPEERASE_PAGES;
+	   EraseInitStruct.PageAddress = StartPage;
+	   EraseInitStruct.NbPages     = ((EndPage - StartPage)/FLASH_PAGE_SIZE) +1;
+
+	   if (HAL_FLASHEx_Erase(&EraseInitStruct, &PAGEError) != HAL_OK)
+	   {
+	     /*Error occurred while page erase.*/
+		  return HAL_FLASH_GetError ();
+	   }
+
+	   /* Program the user Flash area word by word*/
+
+	   while (sofar<numberofwords)
+	   {
+	     if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, StartPageAddress, Data[sofar]) == HAL_OK)
+	     {
+	    	 StartPageAddress += 4;  // use StartPageAddress += 2 for half word and 8 for double word
+	    	 sofar++;
+	     }
+	     else
+	     {
+	       /* Error occurred while writing data in Flash memory*/
+	    	 return HAL_FLASH_GetError ();
+	     }
+	   }
+
+	   /* Lock the Flash to disable the flash control register access (recommended
+	      to protect the FLASH memory against possible unwanted operation) *********/
+	   HAL_FLASH_Lock();
+
+	   return 0;
+}
+
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
 	
 	if(state == 0){
@@ -171,8 +233,26 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart){
   * @brief  The application entry point.
   * @retval int
   */
+
+void Convert_To_Str (uint32_t *data, char *str)
+{
+	int numberofbytes = ((strlen(data)/4) + ((strlen(data) % 4) != 0)) *4;
+
+	for (int i=0; i<numberofbytes; i++)
+	{
+		str[i] = data[i/4]>>(8*(i%4));
+	}
+}
+
+
 int main(void)
 {
+
+  char string[100];
+  char *data = "hello FLASH from ControllerTech This is a test to see how many words can we work with";
+
+
+  uint32_t Rx_Data[30];
   /* USER CODE BEGIN 1 */
 
   /* USER CODE END 1 */
@@ -205,6 +285,12 @@ int main(void)
   
   //TODO décommenter pour voir élément dans la mémoire 
   //HAL_UART_Transmit(&huart2, &datastore, sizeof(datastore), 200);
+
+  int numofwords = (strlen(data)/4)+((strlen(data)%4)!=0);
+  Flash_Write_Data(0x08004810 , (uint32_t *)data, numofwords);
+  Flash_Read_Data(0x08004810 , Rx_Data, numofwords);
+  Convert_To_Str(Rx_Data, string);
+
   
 
   /* USER CODE END 2 */
